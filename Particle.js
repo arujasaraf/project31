3d5503608aeacdb8859b28002db74150799049e4
arujasaraf <65903153+arujasaraf@users.noreply.github.com>
2020-08-03 11:14:51 +0530

    Update Particle.js

--- Particle.js
+++ Particle.js
@@ -1,24 +1,30 @@
-class Particle{
-    constructor(x, y) {
-        var options = {
-            'restitution':0.4,
+class Particle {
+    constructor(x, y,r) {
+
+        var options ={
+            restitution:0.4
         }
-        this.radius=10;
-        this.body =Bodies.circle(x , y , 10 ,options);
-        this.color=color(random(0,255),random(0,255));
+        this.r=r;
+      
+        this.body = Bodies.circle(x, y, this.r,options);       
+        this.color=color(random(0, 255), random(0, 255), random(0, 255));
         World.add(world, this.body);
-        }
-        display(){
-            var pos =this.body.position;
-            var angle =this.body.angle;
-            push();
-            rotate (angle);
-            translate(pos.x,pos.y);
-            noStroke();
-            fill(this.color)
-            ellipseMode(RADIUS);
-            ellipse(0,0, this.radius, this.radius);
-            
-            pop();
-        }
-}
+
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        //imageMode(CENTER);
+        noStroke();
+        fill(this.color)
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.r,this.r);
+        pop();
+    }
+
+};
